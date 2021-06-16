d5a23a8463044436cda10b350c6dd9f04c7e0c59
AbdoulGoudoussy <goudoussy2000@gmail.com>
2021-06-16 21:05:14 +0100

    Base

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+
+app.get("/", function (req, resp){
+    resp.sendFile(__dirname+"/index.html");
+})
+
+app.listen("3000", function(){
+     console.log("Server running");
+})
